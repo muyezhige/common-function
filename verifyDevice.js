df808c38f253655d93eb24895cd44a1358bde328
muyezhige <dehao500@163.com>
2017-08-10 10:57:18 +0800

    Update verifyDevice.js

--- verifyDevice.js
+++ verifyDevice.js
@@ -1,1 +1,28 @@
 
+// 判断是否移动设备
+function isMobileDevice() {
+	var ua = navigator.userAgent.toLowerCase();
+	return !!/(iphone|ios|android|mini|mobile|mobi|nokia|symbian|ipod|ipad|ws\s+phone|mqqbrowser|wp7|wp8|ucbrowser7|ucweb|360\s+aphone\s+browser)/i.test(ua);
+}
+
+// 判断是否IOS
+function isIosDevice() {
+    var u = navigator.userAgent.toLowerCase();
+    var ios = !!u.match(/\(i[^;]+;( U;)? cpu.+mac os x/); //ios终端
+    var iPhone = u.indexOf('iphone') > -1 || u.indexOf('Mac') > -1;
+    var iPad = u.indexOf('ipad') > -1; //是否iPad
+    return !!(ios || iPad || iPhone);
+}
+
+// 判断是否Android
+function isAndroidDevice() {
+    var u = navigator.userAgent.toLowerCase();
+    var android = u.indexOf('android') > -1 || u.indexOf('Linux') > -1;
+    return android;
+}
+
+// 判断是否微信 
+function isWeixin() {
+    var ua = navigator.userAgent.toLowerCase();
+    return "micromessenger" == ua.match(/MicroMessenger/i);
+}
