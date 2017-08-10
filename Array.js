9a79a4af2d4a2d65bedcad16643c5536f5066cee
muyezhige <dehao500@163.com>
2017-08-10 11:09:34 +0800

    Update Array.js

--- Array.js
+++ Array.js
@@ -1,1 +1,13 @@
-
+    /*数组去重
+    *@param sourceArr: 源数组
+    *@param arr : 返回去重后的数组
+    */
+    function unique (sourceArr){
+        var arr  = [];
+        for(var i = 0, len = sourceArr.length; i < len; i++){
+            if($.inArray( sourceArr[i], arr) == -1){
+                arr.push( sourceArr[i] );
+            }
+        }
+        return arr;
+    }
