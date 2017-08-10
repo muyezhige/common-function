3552695a8fc47fa218001cf6243bfe2b6eeb78f6
muyezhige <dehao500@163.com>
2017-08-10 11:10:16 +0800

    Update function.js

--- function.js
+++ function.js
@@ -29,20 +29,6 @@ var Tools = {
         }
     },
 
-    /*数组去重
-    @param sourceArr: 源数组
-    @param arr : 返回去重后的数组
-    */
-    unique: function(sourceArr){
-        var arr  = [];
-
-        for(var i = 0, len = sourceArr.length; i < len; i++){
-            if($.inArray( sourceArr[i], arr) == -1){
-                arr.push( sourceArr[i] );
-            }
-        }
-        return arr;
-    },
 
     /*去除对象中返回undefined的字段*/
     getObj: function(obj){
