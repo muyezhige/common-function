55f089e39d33ede6f4890162b979b40a662b6a5a
muyezhige <dehao500@163.com>
2017-08-10 10:56:56 +0800

    Update function.js

--- function.js
+++ function.js
@@ -11,34 +11,6 @@ function getQueryString(key) {
     return context || '';
 }
 
-// 判断是否移动设备
-function isMobileDevice() {
-	var ua = navigator.userAgent.toLowerCase();
-	return !!/(iphone|ios|android|mini|mobile|mobi|nokia|symbian|ipod|ipad|ws\s+phone|mqqbrowser|wp7|wp8|ucbrowser7|ucweb|360\s+aphone\s+browser)/i.test(ua);
-}
-
-// 判断是否IOS
-function isIosDevice() {
-    var u = navigator.userAgent.toLowerCase();
-    var ios = !!u.match(/\(i[^;]+;( U;)? cpu.+mac os x/); //ios终端
-    var iPhone = u.indexOf('iphone') > -1 || u.indexOf('Mac') > -1;
-    var iPad = u.indexOf('ipad') > -1; //是否iPad
-    return !!(ios || iPad || iPhone);
-}
-
-// 判断是否Android
-function isAndroidDevice() {
-    var u = navigator.userAgent.toLowerCase();
-    var android = u.indexOf('android') > -1 || u.indexOf('Linux') > -1;
-    return android;
-}
-
-// 判断是否微信 
-function isWeixin() {
-    var ua = navigator.userAgent.toLowerCase();
-    return "micromessenger" == ua.match(/MicroMessenger/i);
-}
-
 var Tools = {
     /*事件绑定
     @param node : 代理父节点
