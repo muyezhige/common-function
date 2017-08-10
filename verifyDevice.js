2dc2202e17cf9585a44df290fec4a7eacebd8129
muyezhige <dehao500@163.com>
2017-08-10 11:32:23 +0800

    Update verifyDevice.js

--- verifyDevice.js
+++ verifyDevice.js
@@ -5,7 +5,12 @@ function isMobileDevice() {
 	return !!/(iphone|ios|android|mini|mobile|mobi|nokia|symbian|ipod|ipad|ws\s+phone|mqqbrowser|wp7|wp8|ucbrowser7|ucweb|360\s+aphone\s+browser)/i.test(ua);
 }
 
-// 判断是否IOS
+// 判断是否IOS（1）
+function isIos() {
+    return /iphone|ipad|ipod/.test(navigator.userAgent.toLowerCase());
+}
+
+// 判断是否IOS（2）
 function isIosDevice() {
     var u = navigator.userAgent.toLowerCase();
     var ios = !!u.match(/\(i[^;]+;( U;)? cpu.+mac os x/); //ios终端
